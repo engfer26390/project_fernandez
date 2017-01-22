68b427c1281ef276410d8eaac33e1cb4e74b643d
Josue Velez <josuevelezdr@gmail.com>
2017-01-22 08:19:45 -0400

    ...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,5 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  private
-  def sign_up_params
-    params.require(:user).permit(:email, :password, :password_confirmation,
-    :profile_type, :worker_profile_attributes => [:bio])
-  end
+
 end
